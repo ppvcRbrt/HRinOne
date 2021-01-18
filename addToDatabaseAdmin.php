58ff243e7a945d833a6a6cfe03eb0ca4ad24b811
ppvcRbrt <73713049+ppvcRbrt@users.noreply.github.com>
2021-01-18 15:44:39 +0000

    Made the Admins Add to database page
    Fucking cookies

--- addToDatabaseAdmin.php
+++ addToDatabaseAdmin.php
@@ -22,24 +22,55 @@ $view->questions = $quesQuery->getAll();
     if(isset($_POST["addWorkDom"]))
     {
         setcookie("currentPage", "WorkDomPage");
+        setcookie("sectionAdded", "false");
         header("location:addToDatabaseAdmin.php");
         exit();
     }
     if(isset($_POST["addAssessmentType"]))
     {
+        setcookie("sectionAdded", "false");
         setcookie("currentPage", "AssessmentTypePage");
         header("location:addToDatabaseAdmin.php");
         exit();
     }
     if(isset($_POST["addSection"]))
     {
+        setcookie("sectionAdded", "true");
         setcookie("currentPage", "SectionPage");
         header("location:addToDatabaseAdmin.php");
+
         exit();
+    }
+    else
+    {
+        if(isset($_COOKIE["workDomSelected"]) and isset($_COOKIE["assessmentTypeSelected"]) and strcmp($_COOKIE["sectionAdded"], "false") == 0)
+        {
+            setcookie("workDomSelected", "");
+            setcookie("assessmentTypeSelected", "");
+            header("location:addToDatabaseAdmin.php");
+            //unset($_COOKIE["assessmentTypeSelected"]);
+        }
     }
     if(isset($_POST["addQuestion"]))
     {
         setcookie("currentPage", "QuestionPage");
+        setcookie("sectionAdded", "false");
+        header("location:addToDatabaseAdmin.php");
+        exit();
+    }
+    if(isset($_POST["selectWorkDomain"]))
+    {
+        setcookie("sectionAdded", "true");
+        setcookie("currentPage", "SectionPage");
+        setcookie("workDomSelected", $_POST["workDomNameSect"]);
+        header("location:addToDatabaseAdmin.php");
+        exit();
+    }
+    if(isset($_POST["selectAssessmentType"]))
+    {
+        setcookie("sectionAdded", "true");
+        setcookie("currentPage", "SectionPage");
+        setcookie("assessmentTypeSelected", $_POST["assessmentTypeNameSect"]);
         header("location:addToDatabaseAdmin.php");
         exit();
     }
