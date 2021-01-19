a8e9e251dbef932d5f2343f4421e034f3943bb24
ppvcRbrt <73713049+ppvcRbrt@users.noreply.github.com>
2021-01-19 09:59:53 +0000

    Finalized all the inserts
    Might need to make some small changes, have commented the addToDatabase.php file

--- Models/SectionQueries.php
+++ Models/SectionQueries.php
@@ -28,15 +28,14 @@ class SectionQueries
     {
         $sqlQuery = 'LOCK TABLE Section WRITE;
                      INSERT INTO Section (name, weight, description, assessment_type_ID) 
-                        VALUES (:name, :description, :weight, :assessmentTypeID);
-                     UNLOCK TABLES';
+                        VALUES (:name, :weight, :description, :assessmentTypeID);
+                     UNLOCK TABLES'; //<--Smol mistake that's been fixed
         $statement = $this->_dbHandle->prepare($sqlQuery); // prepare a PDO statement
         $statement->bindValue(':name', $name, PDO::PARAM_STR);
         $statement->bindValue(':weight', $weight, PDO::PARAM_STR);
         $statement->bindValue(':description', $description, PDO::PARAM_STR);
         $statement->bindValue(':assessmentTypeID', $assessmentTypeID, PDO::PARAM_INT);
         $statement->execute(); // execute the PDO statement
-        $statement->debugDumpParams();
 
     }
 
@@ -48,11 +47,7 @@ class SectionQueries
         $statement = $this->_dbHandle->prepare($sqlQuery); // prepare a PDO statement
         $statement->bindValue(':name', $name, PDO::PARAM_STR);
         $statement->execute(); // execute the PDO statement
-        $dataSet = [];
-        while ($row = $statement->fetch()) {
-            $dataSet[] = new SectionTable($row);
-        }
-        return $dataSet;
+        return $statement->fetch();
     }
 
     public function getAll()
