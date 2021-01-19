d65c859dc521ef5579b3861de4d86accf363c577
ppvcRbrt <73713049+ppvcRbrt@users.noreply.github.com>
2021-01-19 12:12:40 +0000

    Final push for this branch
    changed the name of a class

---
+++ createTemplate.php
@@ -0,0 +1,12 @@
+<?php
+require_once('Models/DomainQueries.php');
+require_once('Models/AssessmentTypeQueries.php');
+require_once('Models/SectionQueries.php');
+require_once('Models/QuestionQueries.php');
+
+session_start();
+
+$view = new stdClass();
+
+
+require("Views/createTemplate.phtml");
